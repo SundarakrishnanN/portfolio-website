a807d732a97511a58c2eb5398899100f87360358
SundarakrishnanN <sundar050804@gmail.com>
2025-01-01 09:00:19 +0530

    mobile view fix

--- components/Header.tsx
+++ components/Header.tsx
@@ -7,6 +7,9 @@ import { useState } from 'react'
 export default function Header() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
+  // Function to close the menu
+  const handleLinkClick = () => setIsMenuOpen(false);
+
   return (
     <header className="bg-white shadow-md fixed top-0 left-0 w-full z-50">
       <div className="container mx-auto px-4 py-4 flex justify-between items-center">
@@ -53,6 +56,7 @@ export default function Header() {
                 <li key={item}>
                   <Link
                     href={item === 'Home' ? '/' : `/${item.toLowerCase()}`}
+                    onClick={handleLinkClick}
                     className="text-[#4A5568] hover:text-[#3182CE] transition-colors"
                   >
                     <motion.span
@@ -65,50 +69,56 @@ export default function Header() {
                 </li>
               )
             )}
-            <li>
-              <Link
-                href="https://www.linkedin.com/in/sundarakrishnan-n/"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="text-[#4A5568] hover:text-[#3182CE] transition-colors"
-              >
-                <Linkedin size={20} />
-              </Link>
-            </li>
-            <li>
-              <Link
-                href="https://github.com/SundarakrishnanN/"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="text-[#4A5568] hover:text-[#3182CE] transition-colors"
-              >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="currentColor"
-                  strokeWidth="2"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  className="lucide lucide-github"
+            {/* Icons Section */}
+            <div className="flex items-center space-x-4 justify-center lg:space-x-6 mt-4 lg:mt-0">
+              <li>
+                <Link
+                  href="https://www.linkedin.com/in/sundarakrishnan-n/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="text-[#4A5568] hover:text-[#3182CE] transition-colors"
+                  onClick={handleLinkClick}
                 >
-                  <path d="M15 22v-4a4.8 4.8 0 0 0-1-3.5c3 0 6-2 6-5.5.08-1.25-.27-2.48-1-3.5.28-1.15.28-2.35 0-3.5 0 0-1 0-3 1.5-2.64-.5-5.36-.5-8 0C6 2 5 2 5 2c-.3 1.15-.3 2.35 0 3.5A5.403 5.403 0 0 0 4 9c0 3.5 3 5.5 6 5.5-.39.49-.68 1.05-.85 1.65-.17.6-.22 1.23-.15 1.85v4" />
-                  <path d="M9 18c-4.51 2-5-2-7-2" />
-                </svg>
-              </Link>
-            </li>
-            <li>
-              <Link
-                href="/Sundarakrishnan_Resume.pdf"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="text-[#4A5568] hover:text-[#3182CE] transition-colors"
-              >
-                <FileText size={20} />
-              </Link>
-            </li>
+                  <Linkedin size={20} />
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href="https://github.com/SundarakrishnanN/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="text-[#4A5568] hover:text-[#3182CE] transition-colors"
+                  onClick={handleLinkClick}
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    className="lucide lucide-github"
+                  >
+                    <path d="M15 22v-4a4.8 4.8 0 0 0-1-3.5c3 0 6-2 6-5.5.08-1.25-.27-2.48-1-3.5.28-1.15.28-2.35 0-3.5 0 0-1 0-3 1.5-2.64-.5-5.36-.5-8 0C6 2 5 2 5 2c-.3 1.15-.3 2.35 0 3.5A5.403 5.403 0 0 0 4 9c0 3.5 3 5.5 6 5.5-.39.49-.68 1.05-.85 1.65-.17.6-.22 1.23-.15 1.85v4" />
+                    <path d="M9 18c-4.51 2-5-2-7-2" />
+                  </svg>
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href="/Sundarakrishnan_Resume.pdf"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="text-[#4A5568] hover:text-[#3182CE] transition-colors"
+                  onClick={handleLinkClick}
+                >
+                  <FileText size={20} />
+                </Link>
+              </li>
+            </div>
           </ul>
         </nav>
       </div>
